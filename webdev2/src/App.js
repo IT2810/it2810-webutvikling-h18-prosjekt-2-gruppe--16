0ccedb6a15e5205802aac88f8b7d5be8489b9f8e
auk <oyvind.auk@gmail.com>
2018-09-10 16:20:25 +0200

    adding fakeServerData

--- webdev2/src/App.js
+++ webdev2/src/App.js
@@ -5,7 +5,32 @@ import MediaCategory from "./components/MediaCategory";
 import MediaSelect from "./components/MediaSelect";
 import MediaWrap from "./components/MediaWrap";
 
+
+const mediaData = {
+    img: [
+        {
+            name: "somepic",
+            path: "./media/img/somepic.jpg"
+        }
+    ],
+    audio: [
+        {
+            // id: 1,
+            name: "chant",
+            path: "./media/audio/chant.mp3"
+        }
+    ]
+};
+
+const hei = {hallo: "hei"};
+
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            media: mediaData,
+        };
+    }
   render() {
     return (
       <div className="App">
@@ -13,6 +38,12 @@ class App extends Component {
         <MediaSelect/>
         <MediaWrap/>
         <MediaCategory/>
+
+          { console.log(this.state.media.img) }
+
+          <Renderpic picture={ this.state.media.img[0].name } />
+
+
       </div>
     );
   }
