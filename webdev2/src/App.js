40b401a76c64272a744349971219f23ca676416b
auk <oyvind.auk@gmail.com>
2018-09-18 06:44:49 +0200

    attempting to add radio button functionality

--- webdev2/src/App.js
+++ webdev2/src/App.js
@@ -74,10 +74,26 @@ class App extends Component {
             text={this.state.media.text[this.state.textVar]}
         />
 
-        <MediaCategory />
+        <MediaCategory
+            onRadioButtonClicked={this.handleRadioButtons}
+        />
       </div>
     );
   }
+
+    handleRadioButtons = () => {
+        var rad = document.myForm.myRadios;
+        var prev = null;
+        for(var i = 0; i < rad.length; i++) {
+            rad[i].onclick = function() {
+                (prev)? console.log(prev.value):null;
+                if(this !== prev) {
+                    prev = this;
+                }
+                console.log(this.value)
+            };
+        }
+    };
 
     showTab1 = () => {
         this.setState({imgVar: 0});
--- webdev2/src/components/MediaCategory.js
+++ webdev2/src/components/MediaCategory.js
@@ -4,13 +4,27 @@ class MediaCategory extends Component {
     render() {
         return (
             <div className="MediaCategory">
-                <h4>media category</h4>
 
+                <form name="myForm">
+                    <fieldset>
+                        <legend><strong>Velg et bildetema</strong></legend>
+                        <input type="radio" name="my_options" value="huey" onClick="" />
+
+                        <input type="radio" name="my_options" value="dewey" onClick="" />
+
+                        <input type="radio" name="my_options" value="louie" onClick="" />
+
+                    </fieldset>
+                </form>
+
+
+                <h3>Audio</h3>
                 <input type="radio"/>
-                <input type="radio"/>
+                <h3>Text</h3>
                 <input type="radio"/>
 
             </div>
+
         );
     }
 }
