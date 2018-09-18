d6d0b88b70ff866223b4a25cbd60f15f56237cae
auk <oyvind.auk@gmail.com>
2018-09-18 07:49:31 +0200

    making img dynamicly change

--- webdev2/src/App.js
+++ webdev2/src/App.js
@@ -8,19 +8,51 @@ let media = {
     img: [
         {
             name: 'Light Cocktail lady',
-            path: 'media/img/sosial/1.jpg'
+            path: 'media/img/nature/dam.jpeg'
         },
         {
             name: 'somethimg',
-            path: "media/img/sosial/2.jpg"
+            path: "media/img/nature/kangaroo.jpeg"
         },
         {
             name: 'somethimg more',
+            path: "media/img/nature/koala.jpeg"
+        },
+        {
+            name: 'somethimg morer',
+            path: "media/img/nature/road.jpeg"
+        },
+        {
+            name: 'somethimg morer',
+            path: "media/img/sosial/1.jpg"
+        },
+        {
+            name: 'somethimg morer',
+            path: "media/img/sosial/2.jpg"
+        },
+        {
+            name: 'somethimg morer',
             path: "media/img/sosial/3.jpg"
         },
         {
             name: 'somethimg morer',
             path: "media/img/sosial/4.jpg"
+        },
+        {
+            name: 'somethimg morer',
+            path: "media/img/space/curiosity_descent.jpg"
+        },
+        {
+            name: 'somethimg morer',
+            path: "media/img/space/dual_booster_landing.jpeg"
+        },
+        {
+            name: 'somethimg morer',
+            path: "media/img/space/mars_landing.jpg"
+        },
+        {
+            name: 'somethimg morer',
+            path: "media/img/space/starman.jpeg"
         }
     ],
     audio: [],
@@ -86,28 +118,64 @@ class App extends Component {
         this.setState ({selectedImgTheme: changeEvent.target.value});
     };
 
+
     showTab1 = () => {
-        this.setState({imgVar: 0});
-        this.setState({textVar: 0});
+        if(this.state.selectedImgTheme === 'nature') {
+            this.setState({imgVar: 0});
+        }else if(this.state.selectedImgTheme === 'social') {
+            this.setState({imgVar: 4})
+        }else if(this.state.selectedImgTheme === 'space') {
+            this.setState({imgVar: 8})
+        }
     };
 
     showTab2 = () => {
-        this.setState({imgVar: 1});
-        this.setState({textVar: 1});
-
+        if(this.state.selectedImgTheme === 'nature') {
+            this.setState({imgVar: 1});
+        }else if(this.state.selectedImgTheme === 'social') {
+            this.setState({imgVar: 5})
+        }else if(this.state.selectedImgTheme === 'space') {
+            this.setState({imgVar: 9})
+        }
     };
 
     showTab3 = () => {
-        this.setState({imgVar: 2});
-        this.setState({textVar: 2});
-
+        if(this.state.selectedImgTheme === 'nature') {
+            this.setState({imgVar: 2});
+        }else if(this.state.selectedImgTheme === 'social') {
+            this.setState({imgVar: 6})
+        }else if(this.state.selectedImgTheme === 'space') {
+            this.setState({imgVar: 10})
+        }
     };
 
     showTab4 = () => {
-        this.setState({imgVar: 3});
-        this.setState({textVar: 3});
-
+        if(this.state.selectedImgTheme === 'nature') {
+            this.setState({imgVar: 3});
+        }else if(this.state.selectedImgTheme === 'social') {
+            this.setState({imgVar: 7})
+        }else if(this.state.selectedImgTheme === 'space') {
+            this.setState({imgVar: 11})
+        }
     };
+
+    // showTab2 = () => {
+    //     this.setState({imgVar: 1});
+    //     this.setState({textVar: 1});
+    //
+    // };
+    //
+    // showTab3 = () => {
+    //     this.setState({imgVar: 2});
+    //     this.setState({textVar: 2});
+    //
+    // };
+    //
+    // showTab4 = () => {
+    //     this.setState({imgVar: 3});
+    //     this.setState({textVar: 3});
+    //
+    // };
 }
 
 export default App;
